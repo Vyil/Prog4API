59810ad3adf4c688b1e07ae68c881ea5378bc8c9
Djim <oomesdjim@gmail.com>
2018-06-29 19:28:44 +0200

    editCategorie added

--- controller/categorie_controller.js
+++ controller/categorie_controller.js
@@ -81,15 +81,15 @@ function getCategorieByID(req, res) {
         return
     }
 
-    let urlID= req.params.id
+    let urlID = req.params.id
 
-    db.query('SELECT * FROM categorie WHERE ID = ?',[urlID],function(error,rows,fields){
-        if(!rows[0]){
-            res.status(404).json(new ApiResponse(404,'Niet gevonden (categorieId bestaat niet)')).end()
+    db.query('SELECT * FROM categorie WHERE ID = ?', [urlID], function (error, rows, fields) {
+        if (!rows[0]) {
+            res.status(404).json(new ApiResponse(404, 'Niet gevonden (categorieId bestaat niet)')).end()
         } else {
-            db.query('SELECT * FROM user WHERE ID = ?',[rows[0].UserID], function(err,row,field){
+            db.query('SELECT * FROM user WHERE ID = ?', [rows[0].UserID], function (err, row, field) {
 
-                let responseObject= {
+                let responseObject = {
                     "ID ": urlID,
                     "naam ": rows[0].Naam,
                     "beschrijving ": rows[0].Beschrijving,
@@ -102,8 +102,63 @@ function getCategorieByID(req, res) {
     })
 }
 
+function editCategorie(req, res) {
+    let token = req.get('Authorization')
+    let removeBearer = token.substr(7)
+    let id = auth.decodeToken(removeBearer)
+
+    let categorieID = req.params.id || ''
+    let naam = req.body.naam || ''
+    let beschrijving = req.body.beschrijving || ''
+
+    if(!naam || naam ==''|| !beschrijving || beschrijving==''|| !categorieID || categorieID == ''){
+        res.status(412).json(new ApiResponse(412, 'Een of meer properties in de request body ontbreken of zijn foutief')).end()
+        return
+    }
+    if (!token) {
+        res.status(401).json(new ApiResponse(401, 'Niet geautoriseerd (geen valid token)')).end()
+        return
+    }
+
+    db.query('SELECT * FROM categorie WHERE ID = ?', [categorieID], function (error, rows, fields) {
+        if (!rows[0].UserID == id) {
+            res.status(401).json(new ApiResponse(401, 'Niet geautoriseerd (geen valid token)')).end()
+            return
+        } else {
+            let query = {
+                sql: 'UPDATE categorie SET Naam = ?, Beschrijving = ? WHERE ID = ? ',
+                values: [naam, beschrijving, categorieID],
+                timeout: 3000
+            }
+            db.query(query, function (err, row, field) {
+                if (err) {
+                    res.status(500).json(new ApiResponse(500, err)).end()
+                    return
+                } else {
+                    
+                    db.query('SELECT * FROM user WHERE ID = ?',[id.sub],function(err,row,field){
+                        let responseObject = {
+                            "ID": categorieID,
+                            "naam": naam,
+                            "beschrijving": beschrijving,
+                            "beheerder": row[0].Voornaam,
+                            "email": row[0].Email
+                        }
+                        res.status(200).json(new ApiResponse(200, responseObject)).end()
+
+                    })
+                    
+                }
+            })
+        }
+    })
+
+
+}
+
 module.exports = {
     getAllCategories,
     makeCategorie,
-    getCategorieByID
+    getCategorieByID,
+    editCategorie
 }
--- routes/categorie_routes.js
+++ routes/categorie_routes.js
@@ -4,5 +4,6 @@ let catg = require('../controller/categorie_controller')
 routes.get('/',catg.getAllCategories)
 routes.post('/', catg.makeCategorie)
 routes.get('/:id',catg.getCategorieByID)
+routes.put('/:id',catg.editCategorie)
 
 module.exports = routes
