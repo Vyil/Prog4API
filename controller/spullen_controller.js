1e533ce818e618c5d09383b85cf2e4011dd5dadd
Djim <oomesdjim@gmail.com>
2018-06-29 20:56:21 +0200

    added editSpul function

--- controller/spullen_controller.js
+++ controller/spullen_controller.js
@@ -103,8 +103,69 @@ function getSpulByID(req,res){
 
 }
 
+function editSpul(req,res){
+    let token = req.get('Authorization')
+    let removeBearer = token.substr(7)
+    let id = auth.decodeToken(removeBearer)
+
+    let naam = req.body.naam || ''
+    let beschrijving = req.body.beschrijving || ''
+    let merk = req.body.merk || ''
+    let soort = req.body.soort || ''
+    let bouwjaar = req.body.bouwjaar || ''
+    let categorieID = req.params.id || ''
+    let spulID = req.params.spullid || ''
+
+    if (!token) {
+        res.status(401).json(new ApiResponse(401, 'Niet geautoriseerd (geen valid token)')).end()
+        return
+    }
+    if(!naam || naam ==''|| !beschrijving || beschrijving==''|| !merk || merk==''||
+        !soort || soort ==''|| !bouwjaar || bouwjaar == ''|| !categorieID|| categorieID==''||!spulID || spulID ==''){
+            res.status(412).json(new ApiResponse(412, 'Een of meer properties in de request body ontbreken of zijn foutief')).end()
+            return
+        }
+
+    db.query('SELECT * FROM categorie WHERE ID =?',[categorieID], function(error,rows,fields){
+        if(!rows[0]){
+            res.status(404).json(new ApiResponse(404, 'Niet gevonden (categorieId bestaat niet)')).end()
+            return
+        } else {
+            db.query('SELECT * FROM spullen WHERE ID = ?',[spulID],function(err,row,field){
+                if(!row[0]){
+                    res.status(404).json(new ApiResponse(404, 'Niet gevonden (spullenID bestaat niet)')).end()
+                    return
+                } else {
+                    let query = {
+                        sql: 'UPDATE spullen SET Naam = ?, Beschrijving = ?, Merk = ?, Soort = ?, Bouwjaar = ? WHERE ID = ? ',
+                        values: [naam, beschrijving, merk, soort, bouwjaar, spulID],
+                        timeout: 3000
+                    }
+                    db.query(query, function(error,rows,fields){
+                        if(err){
+                            res.status(500).json(new ApiResponse(500, error)).end()
+                        } else {
+                            let returnObject = {
+                                "ID": spulID,
+                                "naam": naam,
+                                "beschrijving": beschrijving,
+                                "merk": merk,
+                                "soort": soort,
+                                "bouwjaar": bouwjaar
+                            }
+                            res.status(200).json(new ApiResponse(200, returnObject)).end()
+                        }
+                    })
+                }
+            })
+        }
+    })
+
+}
+
 module.exports = {
     getAllSpullen,
     addSpullen,
-    getSpulByID
+    getSpulByID,
+    editSpul
 }
--- routes/spullen_routes.js
+++ routes/spullen_routes.js
@@ -4,6 +4,7 @@ let spullen = require('../controller/spullen_controller')
 routes.get('/:id/spullen',spullen.getAllSpullen)
 routes.post('/:id/spullen',spullen.addSpullen)
 routes.get('/:id/spullen/:spullid',spullen.getSpulByID)
+routes.put('/:id/spullen/:spullid',spullen.editSpul)
 
 
 module.exports = routes
